3ee6a662cecf1640b8652dc569f9f5034323b42f
Alexandre Lavoie <tatti6204@hotmail.com>
2019-03-04 23:27:23 -0500

    Added Projectile.js. Added image of Projectile motion.

--- js/Projectile.js
+++ js/Projectile.js
@@ -1,23 +1,40 @@
 // Model
 var m;
 
-//Objects Array
+// Objects Array
 objects = [];
 
-// Const
+// Constants
+
 const dt = 0.1;
 const lim = 0.99;
 const numOfAgents = 10;
+const accy = 0.1;
+
+// Getting Object Initial Conditions
 
 function getObject(){
-	return [Math.random()*2-1,Math.random()*2-1,0,0,0,0,true];
+	return [-lim+0.01,Math.random()*2-1,Math.random()*0.2,(Math.random()*2-1)*0.2,0,accy,true]; // [px,py,vx,vy,ax,ay,canPredict?]
 }
+
+function getObjects(){
+	temp=[];
+	for(i=0;i<numOfAgents;i++){
+		temp[i]=getObject();
+	}
+	return temp;
+}
+
+// Load Model
 
 async function getModel(json,weight){
 m = await tf.loadModel(tf.io.browserFiles([json.files[0], weight.files[0]]));
 }
 
+// Predict with Loaded Model
+
 async function predict(obj){
+	obj[6] = false;
 	pred = m.predict(tf.tensor2d([obj[0],obj[1],obj[2],obj[3],obj[4],obj[5]],[1,6]));
 	arr = await pred.array();
 	obj[0] = arr[0][0];
@@ -27,21 +44,20 @@ async function predict(obj){
 	obj[6] = true;
 }
 
+// P5JS
+
 function setup() {
 	createCanvas(800, 800);
 	frameRate(30);
 	scale(1,-1);
 	
-	for(i=0;i<numOfAgents;i++){
-		objects[i]=getObject();
-	}
+	objects=getObjects();
 }
 
 function draw() {
 	background(0);
 	noStroke();
 	fill(255,0,0);
-	
 	translate(width/2,height/2);
 	
 	if(m){
@@ -50,15 +66,12 @@ function draw() {
 			ellipse(obj[0]*width/2,obj[1]*height/2,10,10);
 			
 			if(obj[0]>lim||obj[0]<-lim||obj[1]>lim||obj[1]<-lim){
-				obj[0]=Math.random()*2-1;
-				obj[1]=Math.random()*2-1;
-				obj[2]=obj[3]=0;
+				obj[0] = -lim+0.01;
+				obj[1] = Math.random()*2-1;
+				obj[2] = Math.random()*0.2;
+				obj[3] = (Math.random()*2-1)*0.2;
 			}else{
 				if(obj[6]){
-					let d = createVector(((mouseX-width/2)/(width/2))-obj[0],((mouseY-height/2)/(height/2))-obj[1]).normalize().div(10);
-					obj[4] = d.x;
-					obj[5] = d.y;
-					obj[6] = false;
 					predict(obj);
 				}
 			}
